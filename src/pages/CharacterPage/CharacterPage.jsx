b0dabac2b8742667d5a5dfbeea170cfaedf69f59
Dima Zemnitski <g.fredex@gmail.com>
2025-03-15 23:26:49 +0300

    added step fourth

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -4,28 +4,67 @@ import s from './CharacterPage.module.css'
 
 export const CharacterPage = () => {
     const [characters, setCharacters] = useState([])
+    const [info, setInfo] = useState({
+        count: 0,
+        pages: 0,
+        next: null,
+        prev: null,
+    })
+    const [error, setError] = useState(null)
 
+    const fetchData = (url) => {
+        axios.get(url).then((res) => {
+            setCharacters(res.data.results);
+            setInfo(res.data.info);
+            setError(null);
+        })
+            .catch((err) => {
+                setError(err.response.data.error)
+            })
+    }
     useEffect(() => {
         // debugger;
-        axios.get("https://rickandmortyapi.com/api/character").then((res) => {
-            setCharacters(res.data.results)
-        })
+        fetchData("https://rickandmortyapi.com/api/character")
     }, [])
     // debugger;
+
+    const nextPageHandler = () => {
+        fetchData(info.next)
+    }
+
+    const previousPageHandler = () => {
+        fetchData(info.prev)
+    }
+
+    const searchHandler = (event) => {
+        const value = event.currentTarget.value
+        fetchData(`https://rickandmortyapi.com/api/character?name=${value}`)
+    }
+
     return (
         <div className={'pageContainer'}>
             <h1 className={'pageTitle'}>CharacterPage</h1>
-            {characters.length && (
-                <div className={s.characters}>
-                    {characters.map((character) => {
-                        return (
-                            <div key={character.id} className={s.character}>
+            <input type="search" className={s.search} onChange={searchHandler} placeholder="Search..."/>
+            {error && <div className="errorMessage">{error}</div>}
+            {!error && characters.length && (
+                <>
+                    <div className={s.characters}>
+                        {characters.map((character) => {
+                            return (<div key={character.id} className={s.character}>
                                 <div className={s.characterLink}>{character.name}</div>
-                                <img src={character.image} alt={`${character.name} avatar`} />
-                            </div>
-                        )
-                    })}
-                </div>
+                                <img src={character.image} alt={`${character.name} avatar`}/>
+                            </div>)
+                        })}
+                    </div>
+                    <div className={s.buttonContainer}>
+                        <button className="linkButton" disabled={info.prev === null} onClick={previousPageHandler}>
+                            Назад
+                        </button>
+                        <button className="linkButton" disabled={info.next === null} onClick={nextPageHandler}>
+                            Вперед
+                        </button>
+                    </div>
+                </>
             )}
         </div>
     )
