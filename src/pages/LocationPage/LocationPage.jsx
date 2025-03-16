ac0e8dd6a388346b0d590eae437b4ec999be0a93
Dima Zemnitski <g.fredex@gmail.com>
2025-03-17 00:22:42 +0300

    added solution for homeTAsk part1

--- src/pages/LocationPage/LocationPage.jsx
+++ src/pages/LocationPage/LocationPage.jsx
@@ -1,7 +1,55 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 export const LocationPage = () => {
+    const [locations, setLocations] = useState([]);
+    const [info, setInfo] = useState({
+        count: 0,
+        pages: 0,
+        next: null,
+        prev: null,
+    })
+    const [error, setError] = useState(null)
+
+    const fetchData = (url) => {
+        axios.get(url).then((res) => {
+            setLocations(res.data.results);
+            setInfo(res.data.info);
+            setError(null);
+        })
+            .catch((err) => {
+                setError(err.response.data.error)
+            })
+    }
+
+    useEffect(() => {
+        fetchData('https://rickandmortyapi.com/api/location')
+
+    }, []);
+
+    console.log(info);
     return (
-        <div>
+        <div className={'pageContainer'}>
             <h1 className={'pageTitle'}>LocationPage</h1>
-        </div>
+
+            {!error && locations.length && (
+                <>
+                    {locations.map((location) => {
+                        // debugger;
+                        return (
+                            <div>
+                                <ul key={location.id}>
+                                    <li>Название локации: <b>{location.name}</b></li>
+                                    <li>Тип локации: <b>{location.type}</b></li>
+                                    <li>Измерение, в котором находится местоположение: <b>{location.dimension}</b></li>
+                                    <li>Количество персонажей, которых видели в данной
+                                        локации: <b>{location.residents.length}</b></li>
+                                </ul>
+                                <hr/>
+                            </div>)
+                    })}
+                </>
+            )}
+        < /div>
     )
 }
